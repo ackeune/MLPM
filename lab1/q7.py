aee22d32eb7a531f6473da67f3138cfae395926d
Anna <ackeune@gmail.com>
2013-11-09 15:18:54 +0100

    notebook met de 3 covariance plots

---
+++ lab1/q7.py
@@ -0,0 +1,12 @@
+def ICA(X, activation_function, learning_rate):
+    
+    #put x through a linear mapping
+    a = numpy.dot(W,x)
+    
+    #put a through a nonlinear map
+    
+    #put a back through W
+    
+    #adjust the weights
+    
+    return W
